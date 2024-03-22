a7b5eab0e1aad26157d595c719fd978cab9fce96
sergeyuxui4 <163458674+sergeyuxui4@users.noreply.github.com>
2024-03-22 21:29:29 +0300

    homework 2.2

---
+++ lesson01/app.js
@@ -0,0 +1,10 @@
+let firstName ='Sergey'
+let age = '23'
+let city = 'Sochi'
+let hobby = 'photography, design, nature'
+
+console.log('Hello', firstName, age, city, hobby)
+
+
+
+
---
+++ lesson02/app.js
@@ -0,0 +1,33 @@
+const age = 27;
+console.log(typeof age);
+const bigInt = 118943894294n;
+console.log(typeof bigInt)
+const firstName = "Name"
+console.log(typeof firstName)
+
+let age2;
+console.log(age2);
+
+const obj = {
+    name: 'Sergey',
+    age: 23,
+    isWorked: true
+}
+
+const num = 10
+const num2 = 5
+
+console.log(num + num2);
+console.log(num - num2);
+console.log(num * num2);
+console.log(num / num2);
+console.log(num % num2);
+
+const titleProject = 120000
+const screensValue = ("templated, uniquely designed, with animations")
+const screenPrice = 1000
+const percentage = 10
+const fullPrice = 100000
+const responsive = true
+
+
